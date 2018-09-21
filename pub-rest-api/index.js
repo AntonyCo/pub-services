ba7b8b5ed54e14ae47304e4dad031bb1ca004724
Antony <antony.correoso@laposte.net>
2018-09-21 17:48:36 +0200

    first page

--- pub-rest-api/index.js
+++ pub-rest-api/index.js
@@ -2,14 +2,19 @@ const express = require('express');
 const pubService = require('pub-services').services.pubService;
 const app = express();
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/pubs', function (req, res) {
     const pubs = pubService.findAll();
     res.status(200).json(pubs);
 });
 app.get('/pubs/opened', function (req, res) {
     const pubs = pubService.findAllOpened();
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).sendFile('index.html');
+    res.status(200).json(pubs);
 });
 var server = app.listen(3000, function() {
     console.log('Listening on port %d', server.address().port);
